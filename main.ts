998b52a4cf0c460cab9608bd1a065ccc71b1de09
Paul Boon <gvr37.p.l.boon@gmail.com>
2017-12-19 09:40:32 +0100

    p

---
+++ main.ts
@@ -0,0 +1,12 @@
+/// <reference path="node_modules/utilsx/utils.js" />
+/// <reference path="node_modules/vectorx/vector.ts" />
+
+var crret = createCanvas(500,500)
+var canvas = crret.canvas
+var ctxt = crret.ctxt
+
+loop((dt) => {
+    ctxt.clearRect(0,0,500,500)
+
+    ctxt.fillRect(10,10,10,10)
+})
