114fb7e77d64e701b34fd00386a9665a4a0b4139
Paul Boon <gvr37.p.l.boon@gmail.com>
2020-11-09 00:00:31 +0100

    p

--- main.ts
+++ main.ts
@@ -1,5 +1,16 @@
-/// <reference path="node_modules/vectorx/vector.ts" />
-/// <reference path="node_modules/utilsx/utils.ts" />
+/// <reference path="libs/vector/vector.ts" />
+/// <reference path="libs/utils/rng.ts" />
+/// <reference path="libs/utils/store.ts" />
+/// <reference path="libs/utils/table.ts" />
+/// <reference path="libs/utils/utils.ts" />
+/// <reference path="libs/utils/stopwatch.ts" />
+/// <reference path="libs/utils/ability.ts" />
+/// <reference path="libs/utils/anim.ts" />
+/// <reference path="libs/rect/rect.ts" />
+/// <reference path="libs/event/eventqueue.ts" />
+/// <reference path="libs/event/eventsystem.ts" />
+
+
 
 var screensize = new Vector(document.documentElement.clientWidth,document.documentElement.clientHeight)
 var crret = createCanvas(screensize.x,screensize.y)
