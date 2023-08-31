be4b3a31d3448071eb8d27383101c30696ad1212
Paul Boon <gvr37.p.l.boon@gmail.com>
2023-08-31 10:14:07 +0200

    Update ability.ts

--- libs/utils/ability.ts
+++ libs/utils/ability.ts
@@ -1,4 +1,34 @@
 
+class Cooldown{
+    cooldown
+    maxcooldown
+
+    constructor(maxcooldown){
+        this.cooldown = 0
+        this.maxcooldown = maxcooldown
+    }
+
+    isready(){
+        return this.cooldown <= 0
+    }
+
+    tryfire(){
+        if(this.isready()){
+            this.cooldown = this.maxcooldown
+            return true
+        }
+        return false
+    }
+
+    update(dt){
+        this.cooldown -= dt
+        if(this.cooldown < 0){
+            this.cooldown = 0
+        }
+    }
+}
+
+
 class Rule{
 
     constructor(public message:string,public cb:() => boolean){
